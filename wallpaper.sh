68590302bf6152ce8c8ed37bcfff3fe4c77b3ee6
RUKE101 <168942545+RUKE101@users.noreply.github.com>
2024-11-29 13:49:18 +0300

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -8,7 +8,7 @@ echo "ipc = off" >> ~/.config/hypr/hyprpaper.conf
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
 for monitor in $monitors; do
-    wallpaper=$(fd ".png|.jpg|.jpeg" /home/h1st1/Изображения/wallpapers | shuf -n1)
+    wallpaper=$(fd ".png|.jpg|.jpeg" /path/to/your/images | shuf -n1)
     
     # Вывод выбранного обоя
     echo "Monitor: $monitor, Selected Wallpaper: $wallpaper"
