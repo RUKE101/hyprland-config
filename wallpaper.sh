ca7093d5e02195644229d1b00e8f57c9a5fbb491
RUKE101 <168942545+RUKE101@users.noreply.github.com>
2024-11-29 13:52:28 +0300

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -10,7 +10,7 @@ monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 for monitor in $monitors; do
     wallpaper=$(fd ".png|.jpg|.jpeg" /path/to/your/images | shuf -n1)
     
-    # Вывод выбранного обоя
+    
     echo "Monitor: $monitor, Selected Wallpaper: $wallpaper"
 
     echo "preload = $wallpaper" >> ~/.config/hypr/hyprpaper.conf
